1fe5ffb5e184de582b5e19e1a06dcd929563780b
gghendrickx <gghendrickx@gmail.com>
2020-11-21 16:02:03 +0100

    ADDED: loop.py > flowchart for the model simulation loop

--- coral_model/loop.py
+++ coral_model/loop.py
@@ -24,3 +24,66 @@ h = np.ones(4)
 lme = core.Light(I0, Kd, h)
 
 print(lme.I0.shape)
+
+# TODO: Define folder structure
+#  > working directory
+#  > figures directory
+#  > input directory
+#  > output directory
+#  > etc.
+
+# TODO: Model initiation I: Processes and Constants
+#  > specify processes
+#  > specify constants
+
+# TODO: Model initiation II: Environment
+#  > specify environmental factors (i.e. define file names and directories)
+
+# TODO: Model initiation III: Hydrodynamics
+#  > define hydrodynamic module (Delft3D, 1DReef, None)
+#  > initiate hydrodynamic module
+
+# TODO: Model initiation IV: OutputFiles
+#  > specify output files (i.e. define file names and directories)
+#  > specify model data to be included in output files
+
+# TODO: Model initiation V: initial conditions
+#  > specify initial morphology
+#  > specify initial coral cover
+#  > specify carrying capacity
+
+# TODO: Model simulation I: specify SpaceTime
+
+# TODO: Model simulation II: hydrodynamic module
+#  > update hydrodynamics
+#  > extract variables
+
+# TODO: Model simulation III: coral environment
+#  > light micro-environment
+#  > flow micro-environment
+#  > temperature micro-environment
+
+# TODO: Model simulation IV: coral physiology
+#  > photosynthesis
+#  > population states
+#  > calcification
+
+# TODO: Model simulation V: coral morphology
+#  > morphological development
+
+# TODO: Model simulation VI: storm damage
+#  > set variables to hydrodynamic module
+#  > update hydrodynamics and extract variables
+#  > update coral storm survival
+
+# TODO: Model simulation VII: coral recruitment
+#  > update recruitment's contribution
+
+# TODO: Model simulation VIII: return morphology
+#  > set variables to hydrodynamic module
+
+# TODO: Model simulation IX: export output
+#  > write map-file
+#  > write his-file
+
+# TODO: Model finalisation
