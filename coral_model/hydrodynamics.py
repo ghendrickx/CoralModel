0262e32b6524f3c68df2873076071b539fbd8195
gghendrickx <gghendrickx@gmail.com>
2020-12-02 20:23:54 +0100

    ADDED: hydrodynamics > Hydrodynamics: set model (+REFACTOR)

--- coral_model/hydrodynamics.py
+++ coral_model/hydrodynamics.py
@@ -3,6 +3,7 @@ coral_model v3 - hydrodynamics
 
 @author: Gijs G. Hendrickx
 """
+import sys
 
 import numpy as np
 import os
@@ -16,14 +17,24 @@ faulthandler.enable()
 class Hydrodynamics:
     """Interface for all hydrodynamic model modes."""
 
+    __model = None
+
     def __init__(self, mode):
         """
         :param mode: choice of hydrodynamic model
         :type mode: None, str
         """
-        self.mode = self.set_mode(mode)
+        self.mode = self.set_model(mode)
 
-    def set_mode(self, mode):
+    @property
+    def model(self):
+        """Hydrodynamic model.
+
+        :rtype: BaseHydro
+        """
+        return self.__model
+
+    def set_model(self, mode):
         """Function that verifies if the mode is included.
 
         :param mode: choice of hydrodynamic model
@@ -33,6 +44,9 @@ class Hydrodynamics:
         if self.mode not in modes:
             msg = f'{self.mode} not in {modes}.'
             raise ValueError(msg)
+
+        self.__model = getattr(sys.modules[__name__], mode)
+
         return mode
 
     @property
@@ -78,13 +92,16 @@ class Hydrodynamics:
         """Check if all requested content is provided, depending on the mode chosen."""
 
     def initiate(self):
-        pass
+        """Initiate hydrodynamic model."""
+        self.model.initiate()
 
-    def update(self):
-        pass
+    def update(self, mt_per_vt=None):
+        """Update hydrodynamic model."""
+        self.model.update(mt_per_vt=mt_per_vt)
 
     def finalise(self):
-        pass
+        """Finalise hydrodynamic model."""
+        self.model.finalise()
 
 
 class BaseHydro:
@@ -107,16 +124,192 @@ class BaseHydro:
         """Finalise hydrodynamic model."""
 
 
-class Delft3D:
+class Reef0D(BaseHydro):
+    """Explanatory text."""
+
+    def __init__(self):
+        super().__init__()
+
+    def initiate(self):
+        pass
+
+    def update(self, mt_per_vt=None):
+        pass
+
+    def finalise(self):
+        pass
+
+
+class Reef1D(BaseHydro):
+    """Simplified one-dimensional hydrodynamic model over a (coral) reef."""
+    # TODO: Complete the one-dimensional hydrodynamic model
+
+    def __init__(self, bathymetry, wave_height, wave_period, dx=1):
+        """Internal 1D hydrodynamic model for order-of-magnitude calculations on the hydrodynamic conditions on a coral
+        reef, where both flow and waves are included.
+
+        Parameters
+        ----------
+        bathymetry : numeric
+            Bathymetric cross-shore data with means sea level as reference [m]
+            and x=0 at the offshore boundary.
+        wave_height : numeric
+            Significant wave height [m].
+        wave_period : numeric
+            Peak wave period [s].
+        dx : numeric
+            Spatial step between bathymetric data points [m].
+        """
+        super().__init__()
+
+        self.bath = bathymetry
+        self.Hs = wave_height
+        self.Tp = wave_period
+        self.dx = dx
+
+        self.z = np.zeros(self.space)
+
+        self._diameter = None
+        self._height = None
+        self._density = None
+
+    def __str__(self):
+        msg = (
+            f'One-dimensional simple hydrodynamic model to simulate the '
+            f'hydrodynamics on a (coral) reef with the following settings:'
+            f'\n\tBathymetric cross-shore data : {type(self.bath).__name__}'
+            f'\n\t\trange [m]  : {min(self.bath)}-{max(self.bath)}'
+            f'\n\t\tlength [m] : {self.space * self.dx}'
+            f'\n\tSignificant wave height [m]  : {self.Hs}'
+            f'\n\tPeak wave period [s]         : {self.Tp}'
+        )
+        return msg
+
+    def __repr__(self):
+        msg = (
+            f'Reef1D(bathymetry={self.bath}, wave_height={self.Hs}, '
+            f'wave_period={self.Tp})'
+        )
+        return msg
+
+    def initiate(self):
+        pass
+
+    def update(self, mt_per_vt=None):
+        pass
+
+    def finalise(self):
+        pass
+
+    @property
+    def space(self):
+        return len(self.bath)
+
+    @property
+    def x(self):
+        return np.arange(0, self.space, self.dx)
+
+    @property
+    def y(self):
+        return 0
+
+    @property
+    def vel_wave(self):
+        return 0
+
+    @property
+    def vel_curr_mn(self):
+        return 0
+
+    @property
+    def vel_curr_mx(self):
+        return 0
+
+    @property
+    def per_wav(self):
+        return self.Tp
+
+    @property
+    def depth(self):
+        return self.bath + self.z
+
+    @property
+    def can_dia(self):
+        return self._diameter
+
+    @can_dia.setter
+    def can_dia(self, canopy_diameter):
+        self._diameter = canopy_diameter
+
+    @property
+    def can_height(self):
+        return self._height
+
+    @can_height.setter
+    def can_height(self, canopy_height):
+        self._height = canopy_height
+
+    @property
+    def can_den(self):
+        return self._density
+
+    @can_den.setter
+    def can_den(self, canopy_density):
+        self._density = canopy_density
+
+    @staticmethod
+    def dispersion(self, wave_length, wave_period, depth, grav_acc):
+        """Dispersion relation to determine the wave length based on the
+        wave period.
+        """
+        func = wave_length - ((grav_acc * wave_period ** 2) / (2 * np.pi)) * \
+            np.tanh(2 * np.pi * depth / wave_length)
+        return func
+
+    @property
+    def wave_length(self):
+        """Solve the dispersion relation to retrive the wave length."""
+        L0 = 9.81 * self.per_wav ** 2
+        L = np.zeros(len(self.depth))
+        for i, h in enumerate(self.depth):
+            if h > 0:
+                L[i] = fsolve(self.dispersion, L0, args=(self.per_wav, h, 9.81))
+        return L
+
+    @property
+    def wave_frequnecy(self):
+        return 2 * np.pi / self.per_wav
+
+    @property
+    def wave_number(self):
+        k = np.zeros(len(self.wave_length))
+        k[self.wave_length > 0] = 2 * np.pi / self.wave_length[
+            self.wave_length > 0]
+        return k
+
+    @property
+    def wave_celerity(self):
+        return self.wave_length / self.per_wav
+
+    @property
+    def group_celerity(self):
+        n = .5 * (1 + (2 * self.wave_number * self.depth) /
+                  (np.sinh(self.wave_number * self.depth)))
+        return n * self.wave_celerity
+
+
+class Delft3D(BaseHydro):
     """Coupling of coral_model to Delft3D using the BMI wrapper."""
     
     def __init__(self, home_dir, mdu_file, config_file=None):
+        super().__init__()
+
         self.home = home_dir
         self.mdu = mdu_file
         self.config = config_file
         
         self.environment()
-        self.initialize()
+        self.initiate()
         
         self.timestep = None
     
@@ -211,17 +404,17 @@ class Delft3D:
         print(f'\nEnvironment \"PATH\":')
         [print(f'\t{path}') for path in dirs]
         
-    def initialize(self):
+    def initiate(self):
         """Initialize the working model."""
         self.model.initialize()
         
-    def update(self, timestep):
+    def update(self, mt_per_vt=None):
         """Update the working model."""
-        self.timestep = timestep
+        self.timestep = mt_per_vt
         self.reset_counters()
         self.model.update(self.timestep)
     
-    def finalize(self):
+    def finalise(self):
         """Finalize the working model."""
         self.model.finalize()
         
@@ -311,158 +504,6 @@ class Delft3D:
     @can_den.setter
     def can_den(self, canopy_density):
         self.model_fm.set_var('rnveg', canopy_density)
-        
-        
-class Reef1D:
-    """Simplified one-dimensional hydrodynamic model over a (coral) reef."""
-    # TODO: Complete the one-dimensional hydrodynamic model
-    
-    def __init__(self, bathymetry, wave_height, wave_period, dx=1):
-        """
-        Initialisation of `Reef1D`.
-
-        Parameters
-        ----------
-        bathymetry : numeric
-            Bathymetric cross-shore data with means sea level as reference [m]
-            and x=0 at the offshore boundary.
-        wave_height : numeric
-            Significant wave height [m].
-        wave_period : numeric
-            Peak wave period [s].
-        dx : numeric
-            Spatial step between bathymetric data points [m].
-        """
-        self.bath = bathymetry
-        self.Hs = wave_height
-        self.Tp = wave_period
-        self.dx = dx
-        
-        self.z = np.zeros(self.space)
-        
-        self._diameter = None
-        self._height = None
-        self._density = None
-    
-    def __str__(self):
-        msg = (
-            f'One-dimensional simple hydrodynamic model to simulate the '
-            f'hydrodynamics on a (coral) reef with the following settings:'
-            f'\n\tBathymetric cross-shore data : {type(self.bath).__name__}'
-            f'\n\t\trange [m]  : {min(self.bath)}-{max(self.bath)}'
-            f'\n\t\tlength [m] : {self.space * self.dx}'
-            f'\n\tSignificant wave height [m]  : {self.Hs}'
-            f'\n\tPeak wave period [s]         : {self.Tp}'
-        )
-        return msg
-    
-    def __repr__(self):
-        msg = (
-            f'Reef1D(bathymetry={self.bath}, wave_height={self.Hs}, '
-            f'wave_period={self.Tp})'
-        )
-        return msg
-    
-    def update(self, timestep):
-        pass
-    
-    def finalize(self):
-        pass
-    
-    @property
-    def space(self):
-        return len(self.bath)
-    
-    @property
-    def x(self):
-        return np.arange(0, self.space, self.dx)
-    
-    @property
-    def y(self):
-        return 0
-    
-    @property
-    def vel_wave(self):
-        return 0
-    
-    @property
-    def vel_curr_mn(self):
-        return 0
-    
-    @property
-    def vel_curr_mx(self):
-        return 0
-    
-    @property
-    def per_wav(self):
-        return self.Tp
-    
-    @property
-    def depth(self):
-        return self.bath + self.z
-    
-    @property
-    def can_dia(self):
-        return self._diameter
-    
-    @can_dia.setter
-    def can_dia(self, canopy_diameter):
-        self._diameter = canopy_diameter
-    
-    @property
-    def can_height(self):
-        return self._height
-    
-    @can_height.setter
-    def can_height(self, canopy_height):
-        self._height = canopy_height
-    
-    @property
-    def can_den(self):
-        return self._density
-    
-    @can_den.setter
-    def can_den(self, canopy_density):
-        self._density = canopy_density
-        
-    def dispersion(self, wave_length, wave_period, depth, grav_acc):
-        """Dispersion relation to determine the wave length based on the
-        wave period.
-        """
-        func = wave_length - ((grav_acc * wave_period ** 2) / (2 * np.pi)) * \
-            np.tanh(2 * np.pi * depth / wave_length)
-        return func
-    
-    @property
-    def wave_length(self):
-        """Solve the dispersion relation to retrive the wave length."""
-        L0 = 9.81 * self.per_wav ** 2
-        L = np.zeros(len(self.depth))
-        for i, h in enumerate(self.depth):
-            if h > 0:
-                L[i] = fsolve(self.dispersion, L0, args=(self.per_wav, h, 9.81))
-        return L
-    
-    @property
-    def wave_frequnecy(self):
-        return 2 * np.pi / self.per_wav
-    
-    @property
-    def wave_number(self):
-        k = np.zeros(len(self.wave_length))
-        k[self.wave_length > 0] = 2 * np.pi / self.wave_length[
-            self.wave_length > 0]
-        return k
-    
-    @property
-    def wave_celerity(self):
-        return self.wave_length / self.per_wav
-    
-    @property
-    def group_celerity(self):
-        n = .5 * (1 + (2 * self.wave_number * self.depth) /
-                  (np.sinh(self.wave_number * self.depth)))
-        return n * self.wave_celerity
     
     
 if __name__ == '__main__':
