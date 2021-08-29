baf28f3c334c65ced21156484030731a52d8ec54
gghendrickx <gghendrickx@gmail.com>
2021-08-29 18:56:20 +0200

    ADD: hydrodynamics.py > Hydrodynamics: required attributes

--- _v2/hydrodynamics.py
+++ _v2/hydrodynamics.py
@@ -1,5 +1,34 @@
 class Hydrodynamics:
 
+    def __init__(self):
+        self._current_velocity = None
+        self._wave_velocity = None
+        self._wave_period = None
+
     def update(self):
         # TODO: Update hydrodynamic conditions
         pass
+
+    @property
+    def current_velocity(self):
+        """
+        :return: current flow velocity
+        :rtype: float, iterable
+        """
+        return self._current_velocity
+
+    @property
+    def wave_velocity(self):
+        """
+        :return: wave flow velocity
+        :rtype: float, iterable
+        """
+        return self._wave_velocity
+
+    @property
+    def wave_period(self):
+        """
+        :return: wave period
+        :rtype: float, iterable
+        """
+        return self._wave_period
