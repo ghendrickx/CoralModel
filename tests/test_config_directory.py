db9ba96cb1e65b277b71f3dc47ad63ebec5162bb
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2022-07-24 16:35:58 +0200

    DEBUG: test_config_directory.py > TestDirConfig: updated tests.

--- tests/test_config_directory.py
+++ tests/test_config_directory.py
@@ -13,36 +13,36 @@ class TestDirConfig(unittest.TestCase):
 
     def test_default(self):
         folder = DirConfig()
-        self.assertEqual(folder.__repr__(), 'DirConfig(home_dir=None)')
+        self.assertEqual(folder.__repr__(), os.getcwd())
         self.assertEqual(folder.__str__(), os.getcwd())
 
     def test_home_dir1(self):
-        folder = DirConfig(home_dir=r'folder1\folder2')
+        folder = DirConfig(r'folder1\folder2')
         answer = f'{os.getcwd()}\\folder1\\folder2'
         self.assertEqual(folder.__str__(), answer)
 
     def test_home_dir2(self):
-        folder = DirConfig(home_dir=r'C:\folder')
+        folder = DirConfig(r'C:\folder')
         answer = r'C:\folder'
         self.assertEqual(folder.__str__(), answer)
 
     def test_home_dir3(self):
-        folder = DirConfig(home_dir=['folder1', 'folder2'])
+        folder = DirConfig(['folder1', 'folder2'])
         answer = f'{os.getcwd()}\\folder1\\folder2'
         self.assertEqual(folder.__str__(), answer)
 
     def test_home_dir4(self):
-        folder = DirConfig(home_dir=('C:', 'folder'))
+        folder = DirConfig(('C:', 'folder'))
         answer = r'C:\folder'
         self.assertEqual(folder.__str__(), answer)
 
     def test_home_dir5(self):
-        folder = DirConfig(home_dir=[r'C:\folder1', 'folder2'])
+        folder = DirConfig([r'C:\folder1', 'folder2'])
         answer = r'C:\folder1\folder2'
         self.assertEqual(folder.__str__(), answer)
 
     def test_home_dir6(self):
-        folder = DirConfig(home_dir=DirConfig(r'C:\folder'))
+        folder = DirConfig(DirConfig(r'C:\folder'))
         answer = r'C:\folder'
         self.assertEqual(folder.__str__(), answer)
 
@@ -72,26 +72,26 @@ class TestDirConfig(unittest.TestCase):
         self.assertEqual(folder, answer)
 
     def test_home_config_dir1(self):
-        folder = DirConfig(home_dir='folder1').config_dir('folder2')
+        folder = DirConfig('folder1').config_dir('folder2')
         answer = f'{os.getcwd()}\\folder1\\folder2'
         self.assertEqual(folder, answer)
 
     def test_home_config_dir2(self):
-        folder = DirConfig(home_dir=r'folder1\folder2').config_dir(r'folder3\folder4')
+        folder = DirConfig(r'folder1\folder2').config_dir(r'folder3\folder4')
         answer = f'{os.getcwd()}\\folder1\\folder2\\folder3\\folder4'
         self.assertEqual(folder, answer)
 
     def test_home_config_dir3(self):
-        folder = DirConfig(home_dir='C:').config_dir('folder')
+        folder = DirConfig('C:').config_dir('folder')
         answer = r'C:\folder'
         self.assertEqual(folder, answer)
 
     def test_home_config_dir4(self):
-        folder = DirConfig(home_dir=['C:', 'folder1']).config_dir('folder2')
+        folder = DirConfig(['C:', 'folder1']).config_dir('folder2')
         answer = r'C:\folder1\folder2'
         self.assertEqual(folder, answer)
 
     def test_home_config_dir5(self):
-        folder = DirConfig(home_dir=DirConfig(r'C:')).config_dir('folder')
+        folder = DirConfig(DirConfig(r'C:')).config_dir('folder')
         answer = r'C:\folder'
         self.assertEqual(folder, answer)
